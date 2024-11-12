28a91d900785cc612bf7839f73bc1d78a90cc07f
Proutum-Ann <526ALiang@frhsd.com>
2024-11-12 09:30:54 -0500

    added template fpr js

---
+++ script.js
@@ -0,0 +1,13 @@
+let userName = prompt('Who are you')
+let userQuest = prompt('What is your quest?')
+let userFavColor = prompt('What is your favorite color?')
+let swallowAirSpeed = prompt('What is the air speed of an unlaiden swallow?')
+
+
+let myStory = ` <p>Hello ${userName}. Answer me these questions <span class="loud">three</span>, and the other side you'll see.</p>
+
+<p>I see you have already answered, so your quest is to ${userQuest}.</p> `
+
+console.log(myStory)
+
+document.getElementById('story').innerHTML = myStory
