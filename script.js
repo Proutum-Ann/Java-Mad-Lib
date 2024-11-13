0cab2c735ab579c45af182c4ff19ab8f348c8bad
Proutum-Ann <526ALiang@frhsd.com>
2024-11-13 11:44:21 -0500

    first short para done?

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let userName = prompt('Who are you')
-let userQuest = prompt('What is your quest?')
-let userFavColor = prompt('What is your favorite color?')
-let swallowAirSpeed = prompt('What is the air speed of an unlaiden swallow?')
+alert('Hello, welcome to Build-A-Story!')
+let userName = prompt("Let's not dilly-dally any longer, what's your name?")
+let userQuest = prompt(`I see.. your name is ${userName}. Now, what are you here for? Your quest, mayhaps?`)
+let userOcc = prompt("Is that so? Then, what is your occupation?")
+let occInfo = prompt('What does your occupation actually mean?')
+let userFavAnim = prompt("What's your favorite animal?")
+let userNotMons = prompt("What's your least favorite monster?")
 
 
-let myStory = ` <p>Hello ${userName}. Answer me these questions <span class="loud">three</span>, and the other side you'll see.</p>
-
-<p>I see you have already answered, so your quest is to ${userQuest}.</p> `
+let myStory = ` <p>Once upon a time, a young <a class="tooltipster" title="${occInfo}">${userOcc}</a> named ${userName} sets off to ${userQuest} with their pet ${userFavAnim}.</p>
+<p>On their journey to ${userQuest}, they encounter a(n) ${userNotMons}. Because it was a newbie at harassing humans, it was easily defeated.</p> `
 
 console.log(myStory)
 
