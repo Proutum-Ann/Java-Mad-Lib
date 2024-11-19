72edf8b4359402ab8aa4f16bafdfe5f43162de89
Proutum-Ann <526ALiang@frhsd.com>
2024-11-19 11:40:57 -0500

    added a little bit more to the latter section, also added more prompts

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ let userQuest = prompt(`I see.. your name is ${userName}. Now, what are you here
 let userOcc = prompt("Is that so? Then, what is your occupation?")
 let occInfo = prompt('What does your occupation actually mean?')
 let userFavAnim = prompt("What's your favorite animal?")
+let userNotFav = prompt('What\'s your least favorite animal?')
 let userNotMons = prompt("What's your least favorite monster?")
+let charaAge = prompt('How old is your character? (Use "young", "young adult", "old", etc.)')
 
 
-let myStory = ` <p>Once upon a time, a young <a class="tooltipster" title="${occInfo}">${userOcc}</a> named ${userName} sets off to ${userQuest} with their pet ${userFavAnim}.</p>
+let myStory = ` <p>Once upon a time, a ${charaAge} <a class="tooltipster" title="${occInfo}">${userOcc}</a> named ${userName} sets off to ${userQuest} with their pet ${userFavAnim}.</p>
 <p>On their journey to ${userQuest}, they encounter a(n) ${userNotMons}. Because it was a newbie at harassing humans, it was easily defeated. The pair continue on, but tragedy strikes when ${userName}'s pet ${userFavAnim} gets obliterated by some rando claiming to be a wizard. ${userName} manages to blow the rando up.. somehow, but will forever be changed by the loss of their beloved pet.</p> `
-let storySec = ` <p>Lost in thought, ${userName} decides to go home and rest fpr a bit. </p> `
+let storySec = ` <p>Lost in thought, ${userName} decides to go home and rest for a bit. Without their ${userFavAnim}, they feel no motivation to do anything. That was when their friend brought them a new pet ${userNotFav}! It was.. "repulsing", I guess.</p> `
 
 console.log(myStory)
+console.log(storySec)
 
 document.getElementById('story').innerHTML = myStory
+
+document.getElementById('cont').innerHTML = storySec
