ce5f9deafa984099e48f7fb79245516d7c4cc2df
Proutum-Ann <526ALiang@frhsd.com>
2024-11-21 09:03:01 -0500

    story redraft (not outputed yet)

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let charaAge = prompt('How old is your character? (Use "young", "young adult", "
 
 let myStory = ` <p>Once upon a time, a ${charaAge} <a class="tooltipster" title="${occInfo}">${userOcc}</a> named ${userName} sets off to ${userQuest} with their pet ${userFavAnim}.</p>
 <p>On their journey to ${userQuest}, they encounter a(n) ${userNotMons}. Because it was a newbie at harassing humans, it was easily defeated. The pair continue on, but tragedy strikes when ${userName}'s pet ${userFavAnim} gets obliterated by some rando claiming to be a wizard. ${userName} manages to blow the rando up.. somehow, but will forever be changed by the loss of their beloved pet.</p> `
-let storySec = ` <p>Lost in thought, ${userName} decides to go home and rest for a bit. Without their ${userFavAnim}, they feel no motivation to do anything. That was when their friend brought them a new pet ${userNotFav}! It was.. "repulsing", I guess.</p> `
+let storySec = ` <p>Lost in thought, ${userName} decides to go home and rest for a bit. Without their ${userFavAnim}, they feel no motivation to do anything. That was when their friend brought them a new pet ${userNotFav}! It was.. "repulsing", I guess. But it's their new partner now, and nothing will get in their way any longer!</p> `
 
 console.log(myStory)
 console.log(storySec)
@@ -19,3 +19,13 @@ console.log(storySec)
 document.getElementById('story').innerHTML = myStory
 
 document.getElementById('cont').innerHTML = storySec
+
+//You are a ${charaAge} ${charRace} named ${charName}${introRem}
+
+if (charaAge === young){
+    console.log('')
+} else if (charaAge === 'young adult'){
+    console.log(`, a <a class="tooltipster" title="${occInfo}">${charOcc}</a>`)
+} else {
+    console.log(`, a <a class="tooltipster" title="${occInfo}">${charOcc}</a>`)
+}
