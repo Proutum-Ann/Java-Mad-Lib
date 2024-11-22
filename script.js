8f4b861d6a44eb147699b73d0c07a3fcb401a9fd
Proutum-Ann <526ALiang@frhsd.com>
2024-11-22 11:12:16 -0500

    new story now shpws up on website, fixed a few things with strings (too many to count)

--- script.js
+++ script.js
@@ -1,64 +1,59 @@
 alert('Hello, welcome to Build-A-Story!')
-let userName = prompt("Let's not dilly-dally any longer, what's your name?")
-let userQuest = prompt(`I see.. your name is ${userName}. Now, what are you here for? Your quest, mayhaps?`)
-let userOcc = prompt("Is that so? Then, what is your occupation?")
+let charName = prompt("Let's not dilly-dally any longer, what's your name?")
+let charQuest = prompt(`I see.. your name is ${charName}. Now, what are you here for? Your quest, mayhaps?`)
+let charOcc = prompt("Is that so? Then, what is your occupation?")
 let occInfo = prompt('What does your occupation actually mean?')
-let userFavAnim = prompt("What's your favorite animal?")
+let charRace = prompt('What is your character\'s species?')
+let favAnim = prompt("What's your favorite animal?")
 let userNotFav = prompt('What\'s your least favorite animal?')
 let userNotMons = prompt("What's your least favorite monster?")
 let charaAge = prompt('How old is your character? (Use "young", "young adult", "old", etc.)')
-
-
-let myStory = ` <p>Once upon a time, a ${charaAge} <a class="tooltipster" title="${occInfo}">${userOcc}</a> named ${userName} sets off to ${userQuest} with their pet ${userFavAnim}.</p>
-<p>On their journey to ${userQuest}, they encounter a(n) ${userNotMons}. Because it was a newbie at harassing humans, it was easily defeated. The pair continue on, but tragedy strikes when ${userName}'s pet ${userFavAnim} gets obliterated by some rando claiming to be a wizard. ${userName} manages to blow the rando up.. somehow, but will forever be changed by the loss of their beloved pet.</p> `
-let storySec = ` <p>Lost in thought, ${userName} decides to go home and rest for a bit. Without their ${userFavAnim}, they feel no motivation to do anything. That was when their friend brought them a new pet ${userNotFav}! It was.. "repulsing", I guess. But it's their new partner now, and nothing will get in their way any longer!</p> `
-
-console.log(myStory)
-console.log(storySec)
-
-document.getElementById('story').innerHTML = myStory
-
-document.getElementById('cont').innerHTML = storySec
-
 let charGen = prompt('What is the gender of your character? (This is to determine the pronouns used in the story)')
 
 let pnounOne = ''
 let pnounTwo = ''
 let pnounThree = ''
+let pnounFour = ''
 
 let pnounOneOther = ''
 let pnounTwoOther = ''
 let pnounThreeOther = ''
 
-//You are a ${charaAge} ${charRace} named ${charName}${introRem}, who owns a pet ${favAnim}. 
+let introRem = ''
 
-if (charaAge === young){
-    console.log('')
+let myStory = ` <p>Your character is a ${charaAge} ${charRace} named ${charName}${introRem}, who owns a pet ${favAnim}; and ${pnounOne} goal is to ${charQuest}</p> `
+
+
+if (charaAge === 'young'){
+    introRem = ''
 } else if (charaAge === 'young adult'){
-    console.log(`, a <a class="tooltipster" title="${occInfo}">${charOcc}</a>`)
+    introRem = `, a <a class="tooltipster" title="${occInfo}">${charOcc}</a>`
 } else {
-    console.log(`, a <a class="tooltipster" title="${occInfo}">${charOcc}</a>`)
+    introRem = `, a <a class="tooltipster" title="${occInfo}">${charOcc}</a>`
 }
 
-if (charGen === male){
+if (charGen === 'male'){
     pnounOne = 'he';
     pnounTwo = 'him';
-    pnounThree = 'himself'
-} else if (charGen === female){
+    pnounThree = 'his';
+    pnounFour = 'himself'
+} else if (charGen === 'female'){
     pnounOne = 'she';
     pnounTwo = 'her';
-    pnounThree = 'herself'
+    pnounThree = 'her';
+    pnounFour = 'herself'
 } else {
     pnounOne = 'they';
     pnounTwo = 'them';
-    pnounThree = 'themself'
+    pnounThree = 'their';
+    pnounFour = 'themself'
 }
 
-if (charGen === male){
+if (charGen === 'male'){
     pnounOneOther = 'he is';
     pnounTwoOther = 'he was';
     pnounThreeOther = 'himself was';
-} else if (charGen === female){
+} else if (charGen === 'female'){
     pnounOneOther = 'she is';
     pnounTwoOther = 'she was';
     pnounThreeOther = 'herself was';
@@ -67,3 +62,8 @@ if (charGen === male){
     pnounTwoOther = 'they were';
     pnounThreeOther = 'themself were';
 }
+
+
+console.log(story)
+
+document.getElementById('story').innerHTML = myStory
