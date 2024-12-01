207bb4bea9b639264467cebde4e69d4056a8ed87
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2024-12-01 03:20:35 +0000

    finished story, fixed some coding issues, etc. (i can't remember what else i added)

--- script.js
+++ script.js
@@ -33,13 +33,13 @@ let pnounOneOther = ''
 let pnounTwoOther = ''
 let pnounThreeOther = ''
 
-if (charGen === 'male'){
+if (charGen === 'male' || charGen === 'boy'){
     pnounOne = 'he';
     pnounTwo = 'him';
     pnounThree = 'his';
     pnounFour = 'he\'s';
     pnounFive = 'himself'
-} else if (charGen === 'female'){
+} else if (charGen === 'female' || charGen === 'girl'){
     pnounOne = 'she';
     pnounTwo = 'her';
     pnounThree = 'her';
@@ -53,11 +53,11 @@ if (charGen === 'male'){
     pnounFive = 'themself'
 }
 
-if (charGen === 'male'){
+if (charGen === 'male' || charGen === 'boy'){
     pnounOneOther = 'he is';
     pnounTwoOther = 'he was';
     pnounThreeOther = 'himself was';
-} else if (charGen === 'female'){
+} else if (charGen === 'female' || charGen === 'girl'){
     pnounOneOther = 'she is';
     pnounTwoOther = 'she was';
     pnounThreeOther = 'herself was';
@@ -71,12 +71,24 @@ if (charGen === 'male'){
 
 let start = ` <p>Your character is a ${charaAge} ${charRace} named ${charName}${introRem}, who owns a pet ${favAnim}; and ${pnounThree} quest is to ${charQuest}. ${charName} and ${pnounThree} ${favAnim} packed up and got ready for an adventure, of course, not before telling ${pnounThree} family goodbye.</p> `
 
-let adv = ` <p>${charName} now finally sets off with ${pnounThree} ${favAnim} on their grand adventure </p> `
+let adv = ` <p>${charName} now finally sets off with ${pnounThree} ${favAnim} on their grand adventure. Not very long after entering a mysterious forest that surrounded their hometown, they pair finds a ${userNotMons}, seemingly injured. ${charName} tries to help the monster, but ${pnounThree} ${favAnim} had felt that something was off. The ${favAnim} kept trying to remove ${charName} from the ${userNotMons}, and just as ${pnounOne} had enough of ${pnounThree} pet's annoyance, the monster jumps up and strikes ${charName}.</p> <p>${charName}'s ${favAnim} leaps out at the ${userNotMons}, distracting it enough for ${charName} to subdue the monster. What would end up happening to it? Who knows.</p> <p>The adventure duo continue on their journey to ${charQuest}, and all was swell.. mostly.</p> `
 
-console.log(start)
+let pdeath = ` <p>Along the way, the duo comes across a random guy who claims to be a wizard. ${charName} and ${pnounThree} ${favAnim} don't seem to be convinced, however, but little did they know that their lives will be changed by this encounter forever. The wizard was furious, how dare these adventurers doubt him? How dare they even defy him? He waves his wand and casts a spell, obliterating the ${favAnim} right in front of the ${charRace}'s eyes. Feeling like he'd achieved victory, the wizard skedaddles off, never to be seen again. ${charName}, now blaming ${pnounFive} for ${pnounThree} pet's death, heads on to the next town over to meet up with ${pnounThree} friend.</p> `
+
+let npet = ` <p>Upon ${pnounThree} arrival, ${charName}'s friend asks what's happened, as ${pnounOne} seemed depressed. ${charName} explained the journey thus far, and ${pnounThree} seemed to light up a tiny bit. ${charName} asked why the friend had looked to giddy, to which ${pnounThree} friend revealed that they had a surprise for ${pnounTwo}!</p> <p>${charName}'s friend leads ${pnounTwo} to a pen, to which a ${userNotFav} was being held. The friend wanted to gift this animal as a pet to ${charName} as a pet, and was happy that there was a vacancy just in time! Yeah, sort of a bad friend, we know. But ${charName} takes the ${userNotFav} in, reluctantly of course.</p> <p>The newly formed pair heads off into the horizon, perhaps finally being able to ${charQuest} as ${charName} had planned, albeit with a newfound partner. What will become of their party? <i>That</i> will be for your imagination.</p> `
+
+console.log(start) 
 
 document.getElementById('start').innerHTML = start
 
 console.log(adventure)
 
 document.getElementById('adventure').innerHTML = adv
+
+console.log(pdead)
+
+document.getElementById('pdead').innerHTML = pdeath
+
+console.log(npet)
+
+document.getElementById('npet').innerHTML = newpet
